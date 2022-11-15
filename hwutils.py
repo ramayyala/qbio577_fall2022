26d74ee52e5092cf626697dd0fe5f07306958833
Jared Khan <jaredkha@usc.edu>
2022-11-15 15:57:23 -0800

    updated hwutils.py functionality

--- hwutils.py
+++ hwutils.py
@@ -17,26 +17,7 @@ def plot_pca( pca ,
     Can be modified/extended to answer various questions.
     """
     
-    
-    if metadata_label_column is not None:
-        if bigwig_metadata is None: 
-            raise ValueError("must provide metadata table to label by a metadata column") 
-        labels = [bigwig_metadata.query(
-                    "`File accession`==@ file_accession ").loc[:,metadata_label_column].values[0]
-                  for file_accession in pca.feature_names_in_]
-        le = sklearn.preprocessing.LabelEncoder()
-        le.fit(labels)
-        labels = le.transform(labels)
-    else: 
-        labels = None
-        
-    plt.figure(figsize=figsize)
-    plt.scatter(pca.components_[0],
-                pca.components_[1],
-                c = labels,
-                alpha=alpha,
-                lw=lw
-                
+
     if metadata_label_column is not None:
         if bigwig_metadata is None: 
             raise ValueError("must provide metadata table to label by a metadata column") 
@@ -51,11 +32,15 @@ def plot_pca( pca ,
     plt.figure(figsize=figsize)
     ax=plt.scatter(pca.components_[0],pca.components_[1], c= labels, alpha = alpha, lw=lw)
     if labels is not None: 
-            plt.legend(handles=ax.legend_elements()[0], labels = le.classes_.tolist())
+            plt.legend(handles=ax.legend_elements()[0],
+                       labels = le.classes_.tolist(),
+                       prop={'size':6},
+                       loc='upper right',
+                       borderaxespad=0)
             plt.title('PCA By: '+metadata_label_column)
     plt.xlabel("PC1")
     plt.ylabel("PC2")
-    #px.scatter(x=pca.components_[0],y=pca.components_[1], color=labels, width=1000, height=1000, labels={"x":"PC1", "y":"PC2"})
-   
+    
+    
    )
 
